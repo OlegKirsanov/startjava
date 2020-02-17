114cfdb52c0cf35e74c4e5340b08aed6097dab62
Oleg Kirsanov <spoungebender@yandex.ru>
2020-02-17 22:01:43 +0300

    Third Commit

--- Variable.java
+++ Variable.java
@@ -3,7 +3,7 @@ public class Variable {
         byte one = 1;
         short cpu = 4770;
         int ram = 165;
-        long codeProduct = 33110812;
+        long codeProduct = 3311081;
         float freq = 3.43215f;
         double alpha = 5.4159265d;
         char someLetter = 'Z';
