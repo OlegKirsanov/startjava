9adb659430cfae996550f93f124f321cd142040b
Oleg Kirsanov <spoungebender@yandex.ru>
2020-02-17 21:52:35 +0300

    First Commit

---
+++ Variable.java
@@ -0,0 +1,20 @@
+public class Variable {
+    public static void main(String[] args) {
+        byte one = 1;
+        short cpu = 4770;
+        int ram = 16;
+        long codeProduct = 33110812;
+        float freq = 3.43215f;
+        double alpha = 5.4159265d;
+        char someLetter = 'Z';
+        boolean someBoolean = true;
+        System.out.println(one);
+        System.out.println(cpu);
+        System.out.println(ram);
+        System.out.println(codeProduct);
+        System.out.println(freq);
+        System.out.println(alpha);
+        System.out.println(someLetter);
+        System.out.print(someBoolean);
+    }
+}
