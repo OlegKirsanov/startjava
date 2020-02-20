07e54e3842a6f382214b32036338ce9903987e7a
OlegLaptop <spoungebender@yandex.ru>
2020-02-20 18:49:30 +0300

    pool exercise continue

--- Exercises/PoolPuzzleOne.java
+++ Exercises/PoolPuzzleOne.java
@@ -3,12 +3,41 @@ class PoolPuzzleOne {
     int x = 0;
 
     while (x < 4) {
+    	x = x - 1;      	
+      	if (x < 1) {
+      	System.out.print("a ");
+      	}
+      	
+      	if () {
+      	System.out.print("an");
+      	System.out.print(" oyster");
+      	}
 
-      if () {
+		if (x == 1) {
+      	System.out.print("noise");
+      	}
 
-      }
-      System.out.print("")
-
+      	if () {
+      	System.out.print("annoys");
+      	}
+      	
+      	System.out.println("");
+      	x = x + 2;
     }
   }
 }
+
+System.out.print("an");
+
+
+
+
+
+
+a noise annoys an oyster
+"a_" "noise"  "an" "_oyster"
+
+
+"noys_"
+"n_" "oise_"
+"_" 
